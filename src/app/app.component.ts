715a13483c23fdff282e2501d38907475f77f6e4
Mathéo <baune@et.esiea.fr>
2023-03-06 13:46:12 +0100

    [feat] Start MOOC Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'snapface';
-}
+export class AppComponent {}
