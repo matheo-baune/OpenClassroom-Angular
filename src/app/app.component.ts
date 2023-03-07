4a4a5da4dcd44126a1529be5adbe10c81d48ee0b
matheo <matheo.baune@gmail.com>
2023-03-07 22:12:47 +0100

    Suite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,26 @@ import {FaceSnap} from "./models/face-snap.model";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  snap!: FaceSnap;
-  secondSnap!: FaceSnap;
+export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.snap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
+    this.faceSnaps = [{
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 251,
+        location: "Le bord de plage"
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ]
 
-    this.secondSnap = new FaceSnap(
-      'Title',
-      'Description',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      25
-    );
   }
 }
